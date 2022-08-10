9807e64a0015bea0c30496a6757f1f986da4a5ea
OpeDada <opedada@gmail.com>
2022-08-10 16:00:47 +0900

    create first migration and add completed column to table

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/todoapp'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False, default=False)
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
-
-db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -33,7 +34,7 @@ def create_todo():
     print(sys.exc_info())
   finally:
     db.session.close()
-    if error == True:
+    if error:
       abort (400)
     else:
       return jsonify(body)
@@ -54,7 +55,7 @@ def set_completed_todo(todo_id):
 
 @app.route('/')
 def index():
-  return render_template('index.html', data=Todo.query.all())
+  return render_template('index.html', data=Todo.query.order_by('id').all())
 
 
 if __name__ == '__main__':
