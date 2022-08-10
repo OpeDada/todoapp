40c072bb0ed1955872cc7ff6dca812ee29e150ad
OpeDada <opedada@gmail.com>
2022-08-10 12:38:38 +0900

    implement try,except and finally

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/todoapp'
@@ -14,17 +15,29 @@ class Todo(db.Model):
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+# db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  description = request.get_json()['description']
-  todo = Todo(description=description)
-  db.session.add(todo)
-  db.session.commit()
-  return jsonify({
-    'description': todo.description
-  })
+  error = False
+  body = {}
+  try:
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    abort (400)
+  else:
+    return jsonify(body)
+
 
 @app.route('/')
 def index():
