b652b5b718cde88255724f15a4021bb67cd40d2d
OpeDada <opedada@gmail.com>
2022-08-10 14:10:48 +0900

    define the handler for the route

--- app.py
+++ app.py
@@ -38,7 +38,19 @@ def create_todo():
     else:
       return jsonify(body)
 
-@app
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
 
 @app.route('/')
 def index():
